42fe0ad9a20cad4cade9aa770fc50e0ea88b85f4
Richard Derryberry <richardtderryberry@gmail.com>
2022-12-02 22:25:26 -0600

    Initial Commit

---
+++ first_program_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  first_program_test
+//
+//  Created by Richard Derryberry on 12/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
